cc87e9a3a6d5236b9fe377fdbb81ce24bac89dd1
LiuBainian <1029922060@qq.com>
2023-09-13 19:25:19 +0800

    Finish C1 Work 3

---
+++ codes/Chapter 1 - The Basic Grammar/3-3.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+# 创建并读入range函数的相应参数
+lower = int(input())
+upper = int(input())
+step = int(input())
+
+# 请在此添加代码，实现编程要求
+########## Begin ##########
+num = list(range(lower, upper, step))
+print(len(num))
+print(max(num) - min(num))
+
+########## End ##########
+
